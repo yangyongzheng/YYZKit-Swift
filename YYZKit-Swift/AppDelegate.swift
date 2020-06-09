23946e3f8d019ea8f3afb4aef2561b711df75831
yangyongzheng <youngyongzheng@qq.com>
2020-06-09 20:21:51 +0800

    创建工程

---
+++ YYZKit-Swift/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  YYZKit-Swift
+//
+//  Created by Young on 2020/6/9.
+//  Copyright © 2020 Young. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
